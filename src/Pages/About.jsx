7194cf81d5251e5202ae1fe642e2022e9f682541
Adarsh-nihal <adarsh474747@gmail.com>
2023-04-22 15:15:24 +0530

    added

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -17,7 +17,12 @@ const About = () => {
     data-aos-anchor-placement="top-center">
     <h1 >About</h1>
     <h2 id="user-detail-name">I'm Adarsh Gupta</h2>
-    <div style={{color:"white"}} id="user-detail-intro">Observant & detail-oriented aspiring full-stack web developer/MERN developer with a specialization in MERN stack. Judicious & creative when crafting web applications & platforms to propel competitive advantage & revenue growth. Looking forward to honing his skills in a challenging work environment.</div>
+    <div style={{color:"white"}}  id="user-detail-intro">A full stack web developer/MERN developer with 1000+ hours of coading experiance. My objective is simply
+                to be the
+                best web developer that i can be and to contribute to the technology industry all that i know.
+                A quick learner and problem solver who can compete with the fast growing tech industry. Experties in
+                building front end using React.js, Redux and developing the backend using Node.js and Express.js to
+                build a full fledged web applications.</div>
 <p>Drop a mail @ <Link href="mailto:aadarsh474747@gmail.com"><span style={{textDecoration:"underline",color:"salmon",cursor:"pointer"}}>
     adarsh474747@gmail.com </span></Link> <EmailIcon/> </p>
  </div>
