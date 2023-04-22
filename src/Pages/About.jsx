32d472bdaea78acc70249837f5b9db8f4ebd3c24
Adarsh-nihal <adarsh474747@gmail.com>
2023-04-22 15:03:45 +0530

    added

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -17,7 +17,7 @@ const About = () => {
     data-aos-anchor-placement="top-center">
     <h1 >About</h1>
     <h2 id="user-detail-name">I'm Adarsh Gupta</h2>
-    <div style={{color:"white"}} id="user-detail-intro">Observant & detail-oriented aspiring full-stack web developer with a specialization in MERN stack. Judicious & creative when crafting web applications & platforms to propel competitive advantage & revenue growth. Looking forward to honing his skills in a challenging work environment.</div>
+    <div style={{color:"white"}} id="user-detail-intro">Observant & detail-oriented aspiring full-stack web developer/MERN developer with a specialization in MERN stack. Judicious & creative when crafting web applications & platforms to propel competitive advantage & revenue growth. Looking forward to honing his skills in a challenging work environment.</div>
 <p>Drop a mail @ <Link href="mailto:aadarsh474747@gmail.com"><span style={{textDecoration:"underline",color:"salmon",cursor:"pointer"}}>
     adarsh474747@gmail.com </span></Link> <EmailIcon/> </p>
  </div>
